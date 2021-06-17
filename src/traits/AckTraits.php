4c91fdc7c83b6048ba1a9642d3f9efccfb900654
fuyuezhen <976066889@qq.com>
2021-06-17 11:17:41 +0800

    update

--- src/traits/AckTraits.php
+++ src/traits/AckTraits.php
@@ -46,6 +46,7 @@ trait AckTraits
                 Co::sleep(1); // 延迟1秒
                 $ackData = $client->recv(0.2); // 接收消息。只为 WebSocket 使用，需要配合 upgrade() 使用；$timeout 超时时间
                 $ack = json_decode($ackData, true);
+                var_dump($ackData);
                 var_dump($uniqid);
                 var_dump($ack);
                 // 判断是否确认
